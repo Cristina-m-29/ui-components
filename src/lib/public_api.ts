af0edf1e4bb0988700864293430980f3162da70d
Cristina Mititelu <mititelucristina29@gmail.com>
2022-04-27 00:14:26 +0300

    created basic modules for components

--- src/lib/public_api.ts
+++ src/lib/public_api.ts
@@ -1,2 +1,11 @@
-export { ButtonModule } from './button/button.module';
-export * from './index';
+export { McButtonModule } from './button/button.module';
+export { McCardModule } from './card/card.module';
+export { McCheckboxModule } from './checkbox/checkbox.module';
+export { McDatepickerModule } from './datepicker/datepicker.module';
+export { McDialogModule } from './dialog/dialog.module';
+export { McInputModule } from './input/input.module';
+export { McMenuModule } from './menu/menu.module';
+export { McRadioButtonModule } from './radio-button/radio-button.module';
+export { McSelectModule } from './select/select.module';
+
+// export * from './index';
